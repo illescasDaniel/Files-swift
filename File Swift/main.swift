6cb4adee39c02ce8e4d945bc21168fabc1d42aad
Daniel ILLESCAS ROMERO <Daniel@daniel.home>
2016-07-09 15:10:58 +0200

    Initial Commit

---
+++ File Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  File Swift
+//
+//  Created by Daniel Illescas Romero on 09/07/16.
+//  Copyright © 2016 Daniel Illescas Romero. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
